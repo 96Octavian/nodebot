5547dfd132da734876789c10721638955f65509a
Giuseppe Lonardoni <gogol.logog@hotmail.it>
2016-11-30 18:12:41 +0100

    First working version, now live!

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Telegraf = require('telegraf');
 var tumblr = require('tumblr.js');
 var keys = require('./keys')
 var client = keys.client
+var post = {}
+
 program
   .version('0.0.1')
   .option('-T, --Token <TOKEN>', 'Specify the bot TOKEN')
@@ -16,11 +18,37 @@ else {
 	process.exit(1);
 }
 
+var poster = function (ctx) {
+    if (post.text && post.body) {
+        client.createPost('aula-magna', post, function(err, data) {datis = data; erro = err});
+    }
+    else {
+        ctx.reply('Nessun post in coda');
+    }
+}
+
+var texter = function (ctx) {
+    post['text'] = 'text';
+    post['body'] = ctx.message.text.replace('/text ', '');
+    ctx.reply('Post body set');
+}
+
+var titler = function (ctx) {
+    post['title'] = ctx.message.text.replace('/title ', '');
+    ctx.reply('Post title set');
+}
+
 var porter = function (ctx) {
     console.log(ctx.message.text);
-    ctx.reply('ricevuto');
-    console.log(client.blogInfo('peps96.tumblr.com', function (err, 
-data) {}));
+    if (ctx.message.text === '/post') {
+        poster(ctx);
+    }
+    else if (ctx.message.text.substring(0, 6) === '/text ') {
+        texter(ctx);
+    }
+    else if (ctx.message.text.substring(0, 7) === '/title ') {
+        titler(ctx);
+    }
 };
 const bot = new Telegraf(BOT_TOKEN)
 bot.command('start', (ctx) => ctx.reply('Hey'))
