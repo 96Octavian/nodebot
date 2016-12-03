bc34ac6a7d1b56b548afae50d84cfb655722d5e2
Octavian <gogol.logog@hotmail.it>
2016-12-03 11:41:57 +0100

    Fixed per user authentication, first try to get user auth permanent after shutdown

--- tumblrbot.js
+++ tumblrbot.js
@@ -1,12 +1,11 @@
 var program = require('commander');
 const Telegraf = require('telegraf');
 var tumblr = require('tumblr.js');
-var util = require('util');
-var users = { starter: {}}
-
+var fs = require('fs');
+authenticating = JSON.parse(fs.readFileSync('./auth.json'));
 /*Da aggiungere una funzione /blog che con una callback_query cambi il
-blog su cui postare.
-Da definire le varie funzioni per creare i post.
+blog su cui postare
+Da definire le varie funzioni per creare i post
 Controllo errori mancante*/
 
 program
@@ -31,52 +30,78 @@ bot.command('consumer_key', ctx => {
     return ctx.reply(ctx.session.clients);
 })
 bot.command('consumer_secret', ctx => {
-  ctx.session.clients.consumer_secret = ctx.message.text.replace('/consumer_secret ', '');
-  return ctx.reply(ctx.session.clients);
+    ctx.session.clients.consumer_secret = ctx.message.text.replace('/consumer_secret ', '');
+    return ctx.reply(ctx.session.clients);
 })
 bot.command('token_secret', ctx => {
-  ctx.session.clients.token_secret = ctx.message.text.replace('/token_secret ', '');
-  return ctx.reply(ctx.session.clients);
+    ctx.session.clients.token_secret = ctx.message.text.replace('/token_secret ', '');
+    return ctx.reply(ctx.session.clients);
 })
 bot.command('token', ctx => {
-  ctx.session.clients.token = ctx.message.text.replace('/token ', '');
-  return ctx.reply(ctx.session.clients);
+    ctx.session.clients.token = ctx.message.text.replace('/token ', '');
+    return ctx.reply(ctx.session.clients);
 })
 bot.command('allset', ctx => {
-  arr = JSON.parse(ctx.message.text.replace('/allset ', ''));
-  client = tumblr.createClient(arr);
-  client.userInfo(function (err, data) {
-    msg = 'Username: ' + data.user.name + '\nAvailable blogs: ';
-    name = data.user.blogs[0].name;
-    var i;
-    for ( i in data.user.blogs ) {
-      msg += '\n' + data.user.blogs[i].name
-      ctx.session.names.push(data.user.blogs[i].name)
-    }
-    ctx.reply(msg);
-  });
-  return ctx.reply('All set');
-})
-bot.command('set', ctx => {
-  if (Object.keys(ctx.session.clients).length === 4) {
-    client = tumblr.createClient(ctx.session.clients);
-    client.userInfo(function (err, data) {
+    arr = JSON.parse(ctx.message.text.replace('/allset ', ''));
+    ctx.session.client = tumblr.createClient(arr);
+    /*ctx.session.client.userInfo(function (err, data) {
       msg = 'Username: ' + data.user.name + '\nAvailable blogs: ';
       name = data.user.blogs[0].name;
       var i;
       for ( i in data.user.blogs ) {
-        msg += i.name
-        ctx.session.names.push(i.name)
+        msg += '\n' + data.user.blogs[i].name
+        ctx.session.names.push(data.user.blogs[i].name)
       }
       ctx.reply(msg);
-    });
+    });*/
+    identity(ctx);
+    authenticating[ctx.chat.id] = arr;
+    fs.writeFile('./auth.json', JSON.stringify(authenticating), function (err) {
+        if (err) console.log(err);
+        console.log('Done');
+    })
     return ctx.reply('All set');
-  }
 })
+bot.command('set', ctx => {
+    if (Object.keys(ctx.session.clients).length === 4) {
+        ctx.session.client = tumblr.createClient(ctx.session.clients);
+        /*ctx.session.client.userInfo(function (err, data) {
+          msg = 'Username: ' + data.user.name + '\nAvailable blogs: ';
+          name = data.user.blogs[0].name;
+          var i;
+          for ( i in data.user.blogs ) {
+            msg += i.name
+            ctx.session.names.push(i.name)
+          }
+          ctx.reply(msg);
+        });*/
+        identity(ctx);
+        authenticating[ctx.chat.id] = arr;
+        fs.writeFile('./auth.json', JSON.stringify(authenticating), function (err) {
+            if (err) console.log(err);
+            console.log('Done');
+        })
+        return ctx.reply('All set');
+    }
+})
+var identity = function (ctx) {
+    ctx.session.client.userInfo(function (err, data) {
+        msg = 'Username: ' + data.user.name + '\nAvailable blogs: ';
+        name = data.user.blogs[0].name;
+        var i;
+        for (i in data.user.blogs) {
+            msg += '\n' + data.user.blogs[i].name
+            ctx.session.names.push(data.user.blogs[i].name)
+        }
+        return ctx.reply(msg);
+    })
+}
+bot.command('me', ctx => identity(ctx))
+
 bot.command('start', ctx => {
-  ctx.session.clients = ctx.session.clients || {}
-  ctx.session.names = ctx.session.names || []
-  ctx.reply('Hey');
+    ctx.session.clients = ctx.session.clients || {}
+    ctx.session.names = ctx.session.names || []
+    ctx.reply('Hey');
 })
 bot.on('text', (ctx) => {
     ctx.session.counter = ctx.session.counter || 0
